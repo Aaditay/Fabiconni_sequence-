dd67fbfc4c47ef468139db152b362284155d419d
Aaditay <58261116+Aaditay@users.noreply.github.com>
2020-01-06 16:39:10 +0530

    Add files via upload

---
+++ Fabconni.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<int> fabconnni(int x, int y, int n) {
+	std::vector<int> a;
+	a.push_back(x);
+	a.push_back(y);
+	for (int i = 0; i <= n; i++) {
+		int sum = a[i] + a[i + 1];
+		a.push_back(sum);
+	}
+	return(a);
+}
+
+void print(std::string label, std::vector<int> vec) {
+	std::cout << label << std::endl;
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << vec[i] << "\n";
+	}
+
+
+}
+
+int main() {
+	std::string label;
+	std::vector <int> thi;
+	int a, b, n;
+	int sample;
+	std::cout << " Enter the label" << std::endl;
+	std::cin >> label;
+
+	std::cout << " Enter value of a and b " << std::endl;
+
+	std::cin >> a >> b;
+	std::cout << "Eneter number of elements in that            ::::::::       ";
+	std::cin >> n;
+
+
+
+	thi = fabconnni(a, b, n);
+
+	print(label, thi);
+	return 0;
+}
